c18caed148748fb8b36feb33424642adbdfc8186
mnymns <loganmons01@gmail.com>
2024-11-25 23:05:33 -0700

    completed simple lin reg class

---
+++ Lin_Reg/Simple_Lin_Reg.py
@@ -0,0 +1,62 @@
+import stats
+
+class Regression:
+    # create initializer (data is a df)
+    def __init__(self, df):
+        # x & y used for iteration purposes
+        self.x = df.iloc[:, 0]
+        self.y = df.iloc[:, 1]
+        # B1 num & den used to calculate B1
+        B1_num = 0
+        B1_den = 0
+        # avg x & y
+        self.xbar = self.x.mean()
+        self.ybar = self.y.mean()
+
+
+        # loop through all x, y one time to get all data needed, optimized
+        for i in range(len(self.x)):
+            B1_num += stats.get_B1_num(self.x.iloc[i], self.y.iloc[i], self.xbar, self.ybar)
+            B1_den += stats.get_B1_den(self.x.iloc[i], self.xbar)
+
+        # B1 & B0
+        self.B1 = B1_num / B1_den
+        self.B0 = stats.get_B0(self.xbar, self.ybar, self.B1)
+
+        # get rss (residual sum of squares), rse (residual standard error), and tss (total sum of squares)
+        self.rss = 0
+        self.tss = 0
+        # have to iterate again since B0 & B1 must be known
+        for i in range(len(self.x)):
+            self.rss += stats.rss(self.y.iloc[i], self.x.iloc[i], self.B0, self.B1)
+            self.tss += stats.tss(self.y.iloc[i], self.ybar)
+        self.rse = stats.rse(len(self.x), self.rss)
+
+        # standard errors of B1 & B0 (B1_den is the dist from xi to xbar squared total
+        self.se_B1 = stats.se_B1(B1_den, self.rse)
+        self.se_B0 = stats.se_B0(self.xbar, len(self.x), B1_den, self.rse)
+
+        # rsquared value
+        self.r_squared = stats.r_sq(self.rss, self.tss)
+
+
+    # function that gives confidence interval
+    def conf_interval(self, beta):
+        # give user option of 'B0' or 'B1' before calculating
+        if beta == 'B0':
+            return stats.ci_95(self.B0, self.se_B0)
+        else:
+            return stats.ci_95(self.B1, self.se_B1)
+
+    # function for t statistic
+    def t_statistic(self, beta):
+        # give user option
+        if beta == 'B0':
+            return stats.t_stat(self.B0, self.se_B0)
+        else:
+            return stats.t_stat(self.B1, self.se_B1)
+    # p value func
+    def p_value(self, t_stat):
+        return stats.p_value(len(self.x), t_stat)
+
+
--- Lin_Reg/main.py
+++ Lin_Reg/main.py
@@ -1,10 +1,21 @@
 import pandas as pd
+from Simple_Lin_Reg import Regression
 
-class Simple_Lin_Reg:
-    # create initializer (data is a df)
-    def __init__(self, data):
-        self.x = data.iloc[0]
-        self.y = data.iloc[1]
 
-    
+# read file into df
+file = open('Advertising.csv')
+df = pd.read_csv(file, usecols=[1, 4])
+file.close()
+
+model = Regression(df)
+B1 = model.B1
+B0 = model.B0
+t_stat = model.t_statistic(B0)
+
+print('r^2: ', model.r_squared)
+print('t_stat (B0): ', model.t_statistic('B0'))
+print('t_stat (B1): ', model.t_statistic('B1'))
+print('se for B0', model.se_B0)
+print('se for B1', model.se_B1)
+print(f'conf int for B0 & B1: \n {model.conf_interval('B0')}\n {model.conf_interval('B1')}')
 
